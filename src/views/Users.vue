7f84c7a9ff7a9ad8128eb767eb79854069eb0f74
basked <basket.baza@gmail.com>
2021-12-05 23:44:56 +0300

    Rename Auth to Lofin component Change structure projects

--- src/views/Users.vue
+++ src/views/Users.vue
@@ -1,10 +1,26 @@
 <template>
-  <v-data-table
-      :headers="headers"
-      :items="users"
-      :items-per-page="15"
-      class="elevation-1"
-  ></v-data-table>
+  <v-card>
+    <v-card-title>
+
+      <!--      <v-spacer></v-spacer>-->
+      <v-text-field
+          v-model="search"
+          append-icon="mdi-magnify"
+          label="Search"
+          single-line
+          hide-details
+      ></v-text-field>
+    </v-card-title>
+    <v-data-table
+        :headers="headers"
+        :items="users"
+        :items-per-page="15"
+        class="elevation-1"
+        :search="search"
+        :loading=isLoading
+        loading-text="Loading... Please wait"
+    ></v-data-table>
+  </v-card>
 </template>
 
 
@@ -12,6 +28,8 @@
 export default {
   name: "Users",
   data: () => ({
+    search: '',
+    isLoading: false,
     users: [],
     headers: [
       {text: 'Name', value: 'name'},
@@ -19,14 +37,19 @@ export default {
       {text: 'Create', value: 'created_at'},
     ]
   }),
-  computed:{
+  computed: {
     // getUsers(){
     //   return this.$store.dispatch('auth/usersJWT').data
     // }
   },
   created() {
+    this.isLoading = true
     this.$store.dispatch('auth/usersJWT').then((res) => {
-      this.users = res.data.data
+      setTimeout(() => {
+        this.users = res.data.data
+        this.isLoading = false
+      }, 3000);
+
     })
   }
 }
